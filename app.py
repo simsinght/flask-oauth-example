a6381ef5ee0f87ca3eb81421c3e0cc7b418ab3b0
Phill Conrad <pconrad@cs.ucsb.edu>
2015-08-28 13:17:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,12 @@ from flask.ext.login import LoginManager, UserMixin, login_user, logout_user,\
 from oauth import OAuthSignIn
 
 
+from oauthConfig import credentials
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['OAUTH_CREDENTIALS'] = {
-    'facebook': {
-        'id': '470154729788964',
-        'secret': '010cc08bd4f51e34f3f3e684fbdea8a7'
-    },
-    'twitter': {
-        'id': '3RzWQclolxWZIMq5LJqzRZPTl',
-        'secret': 'm9TEd58DSEtRrZHpz2EjrV9AhsBRxKMo8m3kuIZj3zLwzwIimt'
-    }
-}
+app.config['OAUTH_CREDENTIALS'] = credentials
 
 db = SQLAlchemy(app)
 lm = LoginManager(app)
